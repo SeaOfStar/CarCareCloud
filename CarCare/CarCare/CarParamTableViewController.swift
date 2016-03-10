0fdeadc73480b3948d9aec76928f980d7fe54800
SeaOfStar <superluckystar@126.com>
2016-03-10 14:01:09 +0800

    整体参数&续航完成

--- CarCare/CarCare/CarParamTableViewController.swift
+++ CarCare/CarCare/CarParamTableViewController.swift
@@ -32,12 +32,12 @@ class CarParamTableViewController: UITableViewController {
     @IBOutlet weak var baseViewFor地图: UIView!
 
 
-
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.addEdgeForCell()
+
+        self.angelMeter.unitText = "rpm"
 
     }
 
@@ -53,8 +53,11 @@ class CarParamTableViewController: UITableViewController {
     }
 
     private var coreData: [String: AnyObject]!
+
     private var vehicleData: [String : AnyObject]?
     private var driveData: [String : AnyObject]?
+    private var batteryData: [String: AnyObject]?
+    private var locationData: [String: AnyObject]?
 
 
     func reloadWithData(data: [String: AnyObject]?) {
@@ -71,6 +74,15 @@ class CarParamTableViewController: UITableViewController {
                 self.driveData = driveData as? [String: AnyObject]
             }
 
+            // 电池数据
+            if let betteryData = theData["battery"] {
+                self.batteryData = betteryData as? [String: AnyObject]
+            }
+
+            if let locationData = theData["location"] {
+                self.locationData = locationData as? [String: AnyObject]
+            }
+
             self.reload()
         }
     }
@@ -82,14 +94,46 @@ class CarParamTableViewController: UITableViewController {
         // 更新状态面板
         self.reloadStatusPanel()
 
-        // 更新其他参数
-        self.reloadOtherParam()
+        // 更新整体状态
+        self.reloadMainStatus()
+
+        // 更新续航情况
+        self.reloadEndurance()
+
+        // 更新发动机参数
+        self.reloadDrive()
     }
+
+    // MARK: 续航情况
+
+    @IBOutlet weak var 百公里耗电Label: UILabel!
+    @IBOutlet weak var 可续航里程label: UILabel!
+    @IBOutlet weak var 累计里程label: UILabel!
+    
+
+    private func reloadEndurance() {
+
+        NSLog("drive Data = \r\(self.driveData)")
+
+        if let oil = self.driveData?["consumption"] as? Int {
+            let kwh = Double(oil) * 0.1
+            self.百公里耗电Label.text = "\(kwh)度"
+        }
+
+        if let remain = self.driveData?["remaining"] as? Float {
+            self.可续航里程label.text = String(format: "%.2fkm", remain)
+        }
+
+        if let finish = self.driveData?["kilometerage"] as? Float {
+            self.累计里程label.text = String(format: "%.2fkm", finish)
+        }
+    }
+
 
     private func reloadSpeedMeter() {
         if let theDriverInfo = self.driveData {
             if let speed = theDriverInfo["speed"] {
-                let speedValue = speed as! Double
+                let speedValue = speed as! Double * 0.1     // 单位0.1km/h
                 speedMeter.rate = speedValue / 260.0
                 speedMeter.text = "\(speedValue)"
             }
@@ -106,13 +150,62 @@ class CarParamTableViewController: UITableViewController {
         }
 
     }
-    
+
+
+
     private func reloadStatusPanel() {
 
     }
-    
-    private func reloadOtherParam() {
 
+
+    // MARK: 整体状态
+    @IBOutlet weak var 整体车辆状态label: UILabel!
+    @IBOutlet weak var 整体驱动器状态label: UILabel!
+    @IBOutlet weak var 整体电池状态label: UILabel!
+    @IBOutlet weak var 整体定位状态label: UILabel!
+
+    func reloadMainStatus() {
+        整体车辆状态label.text = self.vehicleData?["status"] as? String
+        整体驱动器状态label.text = self.driveData?["status"] as? String
+        整体电池状态label.text = self.batteryData?["status"] as? String
+        整体定位状态label.text = self.locationData?["status"] as? String
+    }
+
+
+
+
+    // MARK: 发动机相关
+    @IBOutlet weak var driverVersionLabel: UILabel!
+    @IBOutlet weak var seriesNumberLabel: UILabel!
+    @IBOutlet weak var driveSpeedLabel: UILabel!
+    @IBOutlet weak var 发动机数量label: UILabel!
+
+
+    private func reloadDrive() {
+
+        if let theDriverInfo = self.driveData {
+
+//            NSLog("\(theDriverInfo)\r\r\r")
+
+            if let versionString = theDriverInfo["softver"] {
+                self.driverVersionLabel.text = versionString as? String
+            }
+
+            if let snString = theDriverInfo["sn"] {
+                self.seriesNumberLabel.text = snString as? String
+            }
+
+            if let speed = theDriverInfo["speed"] {
+                let speedValue = speed as! Double * 0.1 // 单位是0.1 km/h
+                self.driveSpeedLabel.text = "\(speedValue)"
+            }
+
+            // 设备数
+            if let unitsInfo = theDriverInfo["units"] {
+                let units = unitsInfo as! NSArray
+                self.发动机数量label.text = "\(units.count)台设备"
+            }
+        }
     }
 
 
